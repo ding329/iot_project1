c8468396bf4ad3ecc1354329ad498bbb81b8cd4f
ding329 <ding329@yahoo.com>
2016-02-05 17:55:33 -0800

    completed module 11

--- www/js/app.js
+++ www/js/app.js
@@ -10,19 +10,23 @@
     EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
     var service = new EmployeeService();
-    service.initialize().done(function () {
-  router.addRoute('', function() {
-      $('body').html(new HomeView(service).render().$el);
-  });
+    var slider = new PageSlider($('body'));
+    service.initialize().done(function () 
+    {
+        router.addRoute('', function() 
+        {
+            slider.slidePage(new HomeView(service).render().$el);
+        });
 
-  router.addRoute('employees/:id', function(id) {
-      service.findById(parseInt(id)).done(function(employee) {
-          $('body').html(new EmployeeView(employee).render().$el);
-      });
-  });
-
-  router.start();
-});
+        router.addRoute('employees/:id', function(id) 
+        {
+            service.findById(parseInt(id)).done(function(employee) 
+            {
+                slider.slidePage(new EmployeeView(employee).render().$el);
+            });
+        });
+        router.start();
+    });
 
     /* --------------------------------- Event Registration -------------------------------- */
   //  $('.search-key').on('keyup', findByName);
